6ce8c0e03b6c18efbebe18d1da26cbaa139caac4
faysaljafry <faysaljafryofficial@gmail.com>
2021-12-23 21:56:53 +0500

    Admin Dashborad is fully functional

--- app.js
+++ app.js
@@ -31,23 +31,25 @@ mongoClient.connect((err, db) => {
 // create our express app
 const app = express();
 // middleware
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors(corsOptions)); // Use this after the variable declaration
 //Adding following code to App.js will enable the system to serve on
 //Static Ports. This will help heroku to serve pages easily
 
-app.use(express.static(path.join(__dirname, './dist')));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, './dist', 'index.html'));
-});
+// app.use(express.static(path.join(__dirname, './dist')));
+
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, './dist', 'index.html'));
+// });
 // routes
 app.get('/', (req, res) => {
   res.send('Express Server for Survey Application');
 });
 //Route for Admin Login
-app.post('/login?', (req, res) => {
+app.post('/login', (req, res) => {
   const EMAIL = 'faysaljafry@gmail.com';
-  const PASSWORD = '12345';
+  const PASSWORD = '123456';
   console.log('The Body is', req.body);
   const { email, password } = req.body;
   if (email === EMAIL && password === PASSWORD) {
@@ -63,25 +65,53 @@ app.post('/login?', (req, res) => {
       user,
     });
   } else {
-    res.status(404);
+    res.status(403);
     res.json({
       message: 'Wrong Login Information',
     });
   }
 });
-app.post('/saveform', (req, res) => {
-  const questions_collection = client.db('CubeSurvey').collection('questions');
-  questions_collection.insertOne(
-    { title: req.body.questions },
-    function (err, result) {
-      if (err) {
-        console.log(err);
-        res.send('400');
-        return;
-      }
-      res.send('Question were added successfully');
+app.post('/updateSurvey', (req, res) => {
+  console.log('In data Update function');
+  console.log('the id is:' + req.body._id);
+  const SurveyToModify = client.db('CubeSurvey').collection('Forms');
+  let { _id, ...body } = req.body;
+  SurveyToModify.updateOne({ _id }, { $set: body }, function (err, result) {
+    if (err) {
+      console.log(err);
+      res.send('400');
+      return;
     }
-  );
+    res.send(result);
+  });
+});
+
+app.get('/getForms', (req, res) => {
+  console.log('In the get Forms Function');
+  const collection = client.db('CubeSurvey').collection('Forms');
+  collection.find().toArray(function (err, results) {
+    if (err) {
+      console.log(err);
+      res.send('403');
+      return;
+    }
+    res.send(results);
+  });
+});
+app.post('/saveform', (req, res) => {
+  id = new mongo.ObjectId();
+  console.log(id);
+  const collection = client.db('CubeSurvey').collection('Forms');
+  collection.insertOne({ _id: id, ...req.body }, function (err, result) {
+    if (err) {
+      console.log(err);
+      res.send('400');
+      return;
+    }
+    console.log('result is', result);
+    // res.json(result);
+    res.send(result);
+  });
 });
 const port = process.env.PORT || 3000;
 //start server
